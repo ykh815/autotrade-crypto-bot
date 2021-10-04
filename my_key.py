644434bd19fd3b1baa2dacfa327369c9886f130b
KwangHyun Yoo <ykh815@hotmail.com>
2021-10-05 08:25:09 +0900

    remove comment

--- my_key.py
+++ my_key.py
@@ -1,2 +1,2 @@
-upbit_access = "gAAAAABhVS_KNguMr-FN0FkWFspHQZFI4E8gISVQFyFocYK8jyIpy5BdccVkylZLfGcnM7KBqLSUGZWS9TRuCZyTX7GyvPZvSSVBBrMiUFuyP9kLKPolFjOY3eBfbMU3RAkMx1SeLeyp"          # 본인 값으로 변경
-upbit_secret = "gAAAAABhVS_Kh8zHQWUSYcqjkrdBjVy7yB6IVDeG2bePIEnZDtgzw2OFWh8HFsjvwr-LB3Vr7aBfMxLdhvp4lsiLPmSYFpFfMAF9p2n-0pnbdkl6E81f3mhCqSXjAuJlPIMYkG6U3w_A"          # 본인 값으로 변경
+upbit_access = "gAAAAABhVS_KNguMr-FN0FkWFspHQZFI4E8gISVQFyFocYK8jyIpy5BdccVkylZLfGcnM7KBqLSUGZWS9TRuCZyTX7GyvPZvSSVBBrMiUFuyP9kLKPolFjOY3eBfbMU3RAkMx1SeLeyp"
+upbit_secret = "gAAAAABhVS_Kh8zHQWUSYcqjkrdBjVy7yB6IVDeG2bePIEnZDtgzw2OFWh8HFsjvwr-LB3Vr7aBfMxLdhvp4lsiLPmSYFpFfMAF9p2n-0pnbdkl6E81f3mhCqSXjAuJlPIMYkG6U3w_A"
